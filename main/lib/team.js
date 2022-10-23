e0112f1498d424c570521c6c8f2e0dce4048d6b3
Parwiz Haqbien <parwiz.jan03@gmail.com>
2022-10-24 09:40:21 +1100

    adding the team js

---
+++ main/lib/team.js
@@ -0,0 +1,15 @@
+const inquirer = require('inquirer');
+const Engineer = require('./Engineer');
+const Employee = require('./Employess');
+const Intern = require('./Intern');
+const displayPage = require('../html-template');
+
+class team {
+    constructor() {
+        this.member = [];
+    }
+
+    async teamGenerator () {
+        
+    }
+}
