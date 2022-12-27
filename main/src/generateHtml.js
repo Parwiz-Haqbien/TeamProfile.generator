a427a0ad86e4441ead154129e66169837beff4c3
Parwiz-Haqbien <Prwiz.jan03@gmail.com>
2022-12-28 01:05:11 +1100

    created the output  the data will be displayed

--- main/src/generateHtml.js
+++ main/src/generateHtml.js
@@ -46,3 +46,74 @@ function renderEngineer(newEngineer) {
     return ``;
 }
 }
+
+function renderIntern(newIntern) {
+
+
+    if (newIntern) {
+
+        const intern = new Intern(newIntern.name, newIntern.id, newIntern.email, newIntern.school);
+
+        return `<div class="card employee-card">
+        <div class="card-header">
+          <h2 class="card-title">${intern.name}</h2>
+         <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i> ${intern.getRole()}</h3>
+        </div>
+    <div class="card-body">
+      <ul class="list-group">
+        <li class="list-group-item">ID:  ${intern.id} </li>
+        <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}@gmail.com</a></li>
+        <li class="list-group-item">School:  ${intern.school} </li>
+        </ul>
+     </div>
+      </div>
+        </div>
+    </div>
+</div>
+    `;
+    } else {
+        return ``;
+    }
+
+}
+
+//creating function that will show the output html that will be generated
+
+function generateHtml(managerData, allEngineers, allInterns) {
+
+    return ` <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>My Team</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </head>
+    
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3 team-heading">
+                    <h1 class="text-center">My Team</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="team-area col-12 d-flex justify-content-center">
+                ${renderManager(managerData)}
+                ${allEngineers.map(renderEngineer)}
+                ${allInterns.map(renderIntern)}
+                </div>
+            </div>
+        </div>
+    </body>
+    
+    </html>
+  `;
+}
