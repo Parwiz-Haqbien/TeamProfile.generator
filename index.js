0e319f31fe12e9cb846d884ff4fa93250b4cd569
Parwiz-Haqbien <Prwiz.jan03@gmail.com>
2022-12-28 01:13:15 +1100

    added manager questions

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const Engineer = require('./main/lib/Engineer');
 const Intern = require('./main/lib/Intern');
 const fs = require('fs');
@@ -9,5 +10,37 @@ const allIntern = [];
 
 //To store multiple Engineers
 const allEngineers = [];
+
+//create manager questions for user input
+
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'what is your team managers name?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'what is your employee id?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'what is your team managers email address?'
+    },
+    {
+        type: 'input',
+        name: 'number',
+        message: 'what is your team managers office number?'
+    },
+    {
+        type: 'list',
+        message: ' Would you like to add another team member?',
+        name: 'menu',
+        choices: ["Add Engineer", "Add Intern", "Finish"],
+      },
+]
+
 
 
